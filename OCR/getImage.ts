379ee5e16ac8e494bd82d2f1ca8b8c19fa61b66a
alice ferlan <aliceundteles@gmail.com>
2025-03-26 21:21:53 +0900

    feat: add console logs for debugging in fileToGenerativePart and getImageLine functions

--- OCR/getImage.ts
+++ OCR/getImage.ts
@@ -3,6 +3,7 @@ import { ImageData } from "../types";
 // 画像ファイルをBase64エンコードする関数
 // mimeType : image/png, image/jpeg image/gif image/webp etc...
 async function fileToGenerativePart(fileURL: string, mimeType: string): Promise<ImageData> {
+    console.log("run fileToGenerativePart");
     const axios = require("axios");
     const response = await axios.get(fileURL, { responseType: "arraybuffer" });
     const fileBuffer = Buffer.from(response.data);
@@ -15,6 +16,7 @@ async function fileToGenerativePart(fileURL: string, mimeType: string): Promise<
 }
 
 async function getImageLine(messageID: string): Promise<ImageData> {
+    console.log("run getImageLine");
     const getImageURL = `https://api-data.line.me/v2/bot/message/${messageID}/content`
 
     return await fileToGenerativePart(getImageURL, "image/jpeg");
