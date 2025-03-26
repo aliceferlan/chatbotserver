776f14e3302e2772a941141b771a435da1d4e6c2
alice ferlan <aliceundteles@gmail.com>
2025-03-26 21:30:31 +0900

    feat: add authorization header for axios request in fileToGenerativePart function

--- OCR/getImage.ts
+++ OCR/getImage.ts
@@ -3,9 +3,16 @@ import { ImageData } from "../types";
 // 画像ファイルをBase64エンコードする関数
 // mimeType : image/png, image/jpeg image/gif image/webp etc...
 async function fileToGenerativePart(fileURL: string, mimeType: string): Promise<ImageData> {
+    const CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN;
     console.log("run fileToGenerativePart");
     const axios = require("axios");
-    const response = await axios.get(fileURL, { responseType: "arraybuffer" });
+
+    const response = await axios.get(fileURL, {
+        responseType: "arraybuffer",
+        headers: {
+            "Authorization": `Bearer ${CHANNEL_ACCESS_TOKEN}`
+        }
+    });
     const fileBuffer = Buffer.from(response.data);
     console.log("fileBuffer", fileBuffer);
     return {
